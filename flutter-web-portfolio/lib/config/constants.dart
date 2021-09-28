e31613ff952a5ebb6e43a71434b9dfd037c0c5b4
muhd ameen <ameens.in19@gamil.com>
2021-09-28 12:19:00 +0530

    flutter web files added

---
+++ flutter-web-portfolio/lib/config/constants.dart
@@ -0,0 +1,20 @@
+mixin AppConstants {
+  static final String mail = 'ameens.in19@gmail.com';
+
+  static final String phone = '(+91) 97 46 67 03 22';
+
+  static final String location = 'Calicut, Kerala, India';
+
+  static final String github = 'https://github.com/muhd-ameen';
+
+  static final String linkedin =
+      'https://www.linkedin.com/in/muhd-ameen/';
+
+  static final String twitter = 'https://twitter.com/AmeenSulaiman4';
+
+  static final String facebook =
+      'https://www.facebook.com/';
+
+  static final String cv =
+      'https://www.canva.com/design/DAEpgHdqFfU/Nw7Rlny2mDaV-jFYG7GHUQ/view?';
+}
